2000e69925aaf2e2f6f79aba24619bb0de6f3cf3
Oleksii Borysonok <alex.borysonok@gmail.com>
2021-02-17 01:39:02 -0500

    changed paths to relative in routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -10,7 +10,7 @@ import AppLegalService from '../app-legal-service';
 import AppSchedule from '../app-schedule';
 import AppOrderForm from '../app-order-form/app-order-form';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import './app.css';
 
@@ -23,28 +23,29 @@ export default class App extends Component {
             <AppHeader />
           </div>
           <div>
-            <Route path="/" exact component={AppMain} />
-            <Route path="/main" component={AppMain} />
-            <Route path="/about" component={AppAbout} />
-            <Route path="/training" exact component={AppTraining} />
-            <Route
-              path="/training/:id"
-              render={({ match }) => {
-                const { id } = match.params;
-                return (<AppTraining trainingId={id} />);
-              }}
-            />
-            <Route path="/contacts" component={AppContacts} />
-            <Route path="/reviews" component={AppReviews} />
-            <Route path="/legal-service" component={AppLegalService} />
-            <Route path="/schedule" component={AppSchedule} />
-            <Route
-              path="/order-form"
-              render={({ match }) => {
-
-                return (<AppOrderForm orderTitle="==== TEST TITLE ====" />);
-
-              }} />
+            <Switch>
+              <Route path="/" exact component={AppMain} />
+              <Route path="/main/" component={AppMain} />
+              <Route path="/about/" component={AppAbout} />
+              <Route path="/training/" exact component={AppTraining} />
+              <Route
+                path="/training/:id"
+                render={({ match }) => {
+                  const { id } = match.params;
+                  return (<AppTraining trainingId={id} />);
+                }}/>
+              <Route path="/contacts/" component={AppContacts} />
+              <Route path="/reviews/" component={AppReviews} />
+              <Route path="/legal-service/" component={AppLegalService} />
+              <Route path="/schedule/" component={AppSchedule} />
+              <Route
+                path="/order-form/"
+                render={(params) => {
+                  //console.log("===== params: ", params);
+                  return (<AppOrderForm orderTitle="==== TEST TITLE ====" />);
+                }} />
+              <Redirect to='/' />
+            </Switch>
           </div>
           <div className="p-2 bg-light border">
             <AppFooter />
